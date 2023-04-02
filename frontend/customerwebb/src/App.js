ab356889bb4cce14fa7aa0464e38ab8722ca91ab
alicebrallis <aliceolsson_@hotmail.com>
2023-04-02 17:07:04 +0200

    .

--- frontend/customerwebb/src/App.js
+++ frontend/customerwebb/src/App.js
@@ -1,54 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import fetchModel from './models/model';
-import Navbar from './components/Navbar';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import fetchModel from "./models/model";
+import Navbar from "./components/Navbar";
 import Router from "./components/Routes";
 
 function App() {
-  let [content, setContent] = useState("")
+    let [content, setContent] = useState("");
 
-  useEffect(() => {
-    (async () => {
-      let fetch = await fetchModel.fetchResult()
-      setContent(fetch)
-        
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            let fetch = await fetchModel.fetchResult();
+            setContent(fetch);
+        })();
+    }, []);
 
-  const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-  const localStorageValue = localStorage.getItem("loggedIn");
-  if (localStorageValue === "true") {
-    setLoggedIn(true);
-  }
-}, []);
-  const handleLogin = () => {
-    setLoggedIn(true);
-    localStorage.setItem("loggedIn", true);
-    window.location.href = "http://localhost:1337/login?origin=1340";
-  };
+    const [loggedIn, setLoggedIn] = useState(false);
+    useEffect(() => {
+        const localStorageValue = localStorage.getItem("loggedIn");
+        if (localStorageValue === "true") {
+            setLoggedIn(true);
+        }
+    }, []);
+    const handleLogin = () => {
+        setLoggedIn(true);
+        localStorage.setItem("loggedIn", true);
+        window.location.href = "http://localhost:1337/login?origin=1340";
+    };
 
-  const handleLogout = () => {
-    setLoggedIn(false);
-    localStorage.removeItem("loggedIn");
-    window.location.href = "http://localhost:1337/logout?origin=1340";
-  };
-  return (
-    <div className="App">
-      <Navbar />
-      <Router />
-        {content.idtest_table}
-      {loggedIn ? (
-        <button className="login-button" onClick={handleLogout}>
-          Logga ut
-        </button>
-      ) : (
-        <button className="login-button" onClick={handleLogin}>
-          Logga in med Google
-        </button>
-      )}
-    </div>
-  );
+    const handleLogout = () => {
+        setLoggedIn(false);
+        localStorage.removeItem("loggedIn");
+        window.location.href = "http://localhost:1337/logout?origin=1340";
+    };
+    return (
+        <div className='App'>
+            <Navbar />
+            <Router />
+            {content.idtest_table}
+            {loggedIn ? (
+                <button className='login-button' onClick={handleLogout}>
+                    Logga ut
+                </button>
+            ) : (
+                <button className='login-button' onClick={handleLogin}>
+                    Logga in med Google
+                </button>
+            )}
+        </div>
+    );
 }
 
 export default App;
--- frontend/customerwebb/src/models/model.js
+++ frontend/customerwebb/src/models/model.js
@@ -1,22 +1,71 @@
 const fetching = {
     fetchResult: async function fetchResult() {
+        const response = await fetch(`http://localhost:1337`);
+        const result = await response.json();
+
+        return result[0];
+    },
+
+    customers: async function customers() {
         const response = await fetch(`http://localhost:1337/api/v01/user`, {
             credentials: "include",
         });
         const result = await response.json();
-        //console.log(result);
 
-        return result[0];
+        return result;
     },
-    customers: async function customers() {
-        const response = await fetch(`http://localhost:1337/api/v01/user/`);
+
+    accounts: async function accounts() {
+        const response = await fetch(`http://localhost:1337/api/v01/payment`, {
+            credentials: "include",
+        });
         const result = await response.json();
-        // console.log(result);
-        try {
-            return result[0].user_id;
-        } catch (error) {
-            throw `Constellation with id of ${error} could not be found.`;
-        }
+
+        return result;
+    },
+    addAccount: async function addAccount(cardDetails) {
+        const response = await fetch(`http://localhost:1337/api/v01/payment/add/account`, {
+            credentials: "include",
+            method: "POST",
+            body: JSON.stringify(cardDetails),
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+        const result = await response.json();
+        console.log(result);
+
+        return result;
+    },
+
+    addBalanceToAccount: async function addBalanceToAccount(balanceAmount) {
+        const response = await fetch(`http://localhost:1337/api/v01/payment`, {
+            credentials: "include",
+            method: "PUT",
+            body: JSON.stringify(balanceAmount),
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+        const result = await response;
+        console.log(result.status);
+
+        return result.status;
+    },
+
+    monthlyPayment: async function monthlyPayment(yesno) {
+        const response = await fetch(`http://localhost:1337/api/v01/payment/monthly-payment`, {
+            credentials: "include",
+            method: "PUT",
+            body: JSON.stringify(yesno),
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+        const result = await response;
+        console.log(result.status);
+
+        return result;
     },
 };
 
